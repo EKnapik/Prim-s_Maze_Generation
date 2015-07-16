7e578cd6084282f70e42f2ddcb3e325068a0ab05
Eric Knapik <eric11mk@gmail.com>
2015-07-15 19:18:47 -0700

    THere is a bug but the main code for prim is down just have to setup the rendering and possibly switch to GLFW because GLUT has weird rules on when it makes the window visible

--- src/main.c
+++ src/main.c
@@ -14,7 +14,9 @@
 #include <OpenGL/gl.h>
 
 #include <stdlib.h>
+#include <stdbool.h>
 #include <stdio.h>
+#include <time.h>
 
 // define some paramaters as the default for the maze
 #define MazeRows 20
@@ -23,20 +25,36 @@
 #define WindowHeight 750
 #define WindowWidth 750
 
-// Globals
+#define TOP 0
+#define RIGHT 1
+#define BOTTOM 2
+#define LEFT 3
 
+/*
+*   top is edge 0
+*   right is edge 1
+*   bottom is edge 2
+*   left is edge 3
+*/
+typedef struct WallStruct {
+    unsigned int row : 15; // 15 bits allocated
+    unsigned int col : 15; // 15 bits allocated
+    unsigned int edge : 2; // 2 bits allocated (I only need 4 values)
+} Wall; // total is 4 bytes
+
+
+// Globals
+_Bool maze[MazeRows][MazeCols];
+int totalCells = MazeRows * MazeCols;
+int cellsInMaze = 0;
+Wall *wallArray;
+int wallArraySize = 0;
 
 // functions
 void setupGLUT( int argc, char *argv[] );
 void render( void );
-
-
-typedef struct WallStruct {
-    unsigned int row : 15 // 15 bits allocated
-    unsigned int col : 15 // 15 bits allocated
-    unsigned int edge : 2 // 2 bits allocated (I only need 4 values)
-} Wall; // total is 4 bytes
-
+void addWalls( int row, int col );
+void setNextPosition( int *curRow, int *curCol );
 
 // Main function
 // This could have user input for maze size
@@ -83,7 +101,7 @@ void render( void )
     glClear( GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT );
 
     // Maze and rules for maze code:
-    _Bool maze[MazeRows][MazeCols];
+    wallArraySize = 0;
     for( int row = 0; row < MazeRows; row++ )
     {
         for( int col = 0; col < MazeCols; col++ )
@@ -92,17 +110,11 @@ void render( void )
         }
     }
 
-    int totalCells = MazeRows * MazeCols;
-    int cellsInMaze = 0;
-
-    Wall *wallArray;
-    int wallArraySize = 0;
-
     // Initialize first cell to be added
     int curRow = 0;
     int curCol = 0;
     maze[curRow][curCol] = true; // this cell is now in the maze
-    wallArraySize = addWalls( *wallArray, wallArraySize, curRow, curCol );
+    addWalls( curRow, curCol );
     // render current position as red
 
     // Enter the the minimum spanning tree loop of adding cells to the maze
@@ -110,12 +122,119 @@ void render( void )
     {
         // Render current position as white
         // Determine next position by random choice from wall Array
-        // Set that position as current
+        setNextPosition( &curRow, &curCol );
         // set maze at current position to true meaning it is in the maze
+        maze[curRow][curCol] = true; // this cell is now in the maze
         // add current position's walls to the maze
+        addWalls( curRow, curCol );
         // render current position as red
     }
 
     // clean up pointers
     free( wallArray );
+}
+
+
+void addWalls( int row, int col )
+{
+    if( wallArraySize < 1 )
+    {
+        wallArray = malloc( sizeof(Wall) );
+    }
+
+    Wall wall;
+    // try and add top
+    if( (row-1) > 0 )
+    {
+        wall.row = row;
+        wall.col = col; 
+        wall.edge = TOP;
+        wallArraySize++;
+        wallArray = realloc(wallArray, (sizeof(Wall) * wallArraySize));
+        wallArray[wallArraySize-1] = wall;
+    }
+    // try and add bottom
+    if( (row+1) < MazeRows )
+    {
+        wall.row = row;
+        wall.col = col; 
+        wall.edge = BOTTOM;
+        wallArraySize++;
+        wallArray = realloc(wallArray, (sizeof(Wall) * wallArraySize));
+        wallArray[wallArraySize-1] = wall;
+    }
+    // try and add left
+    if( (col-1) > 0 )
+    {
+        wall.row = row;
+        wall.col = col; 
+        wall.edge = LEFT;
+        wallArraySize++;
+        wallArray = realloc(wallArray, (sizeof(Wall) * wallArraySize));
+        wallArray[wallArraySize-1] = wall;
+    }
+    // try and add right
+    if( (col+1) < MazeCols )
+    {
+        wall.row = row;
+        wall.col = col; 
+        wall.edge = RIGHT;
+        wallArraySize++;
+        wallArray = realloc(wallArray, (sizeof(Wall) * wallArraySize));
+        wallArray[wallArraySize-1] = wall;
+    }
+}
+
+
+
+void setNextPosition( int *curRow, int *curCol )
+{
+    srand(time(NULL));
+    int randomNum; 
+    Wall wall;
+
+    while( wallArraySize > 0 )
+    {
+        randomNum = rand() % wallArraySize;    
+        wall = wallArray[randomNum]; // get the wall at random place
+        wallArraySize--; // I just removed a wall
+        wallArray[randomNum] = wallArray[wallArraySize]; // replace position with last wall
+        wallArray = realloc(wallArray, (sizeof(Wall) * wallArraySize)); // resize the array
+        if( TOP == wall.edge )
+        {
+            if( !maze[wall.row+1][wall.col] ) // all non added cells are false
+            {
+                *curRow = wall.row+1;
+                *curCol = wall.col;
+                return;
+            }
+        }
+        else if( BOTTOM == wall.edge )
+        {
+            if( !maze[wall.row-1][wall.col] ) 
+            {
+                *curRow = wall.row-1;
+                *curCol = wall.col;
+                return;
+            }
+        }
+        else if( RIGHT == wall.edge )
+        {
+            if( !maze[wall.row][wall.col+1] ) 
+            {
+                *curRow = wall.row;
+                *curCol = wall.col+1;
+                return;
+            }
+        }
+        else
+        {
+            if( !maze[wall.row][wall.col-1] ) 
+            {
+                *curRow = wall.row;
+                *curCol = wall.col-1;
+                return;
+            }
+        }
+    }
 }
